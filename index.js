33a0133558fa02859a5001ab9d63ac643244d855
pulsedevelopers2 <71938596+pulsedevelopers2@users.noreply.github.com>
2021-11-01 13:13:58 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const serverless = require('serverless-http');
-const {getCategories, } = require('./endpointHandlers/catalog')
+const {getCategories, getItems } = require('./endpointHandlers/catalog')
 
 const app = express()
 const router = express.Router();
@@ -10,8 +10,6 @@ app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
-
-
 
 // API's
 router.get('/getCategories', getItems);
